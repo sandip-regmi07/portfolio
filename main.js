0be3798145bf3ca61236ea5e9859e2978d5deb49
Sandip Regmi <regmisandeep32@gmail.com>
2026-08-11 18:44:30 +0545

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var typed = new Typed(".text", {
+
+    strings: [
+        "BIM Student",
+        "Web Developer",
+        "Frontend Developer",
+        "Programmer"
+    ],
+
+    typeSpeed: 100,
+
+    backSpeed: 70,
+
+    backDelay: 1000,
+
+    loop: true
+
+});
