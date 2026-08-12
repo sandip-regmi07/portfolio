094abc2682206604ad0dd497e8fa7d8d5cd2da12
Sandip Regmi <regmisandeep32@gmail.com>
2026-08-12 18:51:31 +0545

    Add menu toggle and dark mode functionality

--- main.js
+++ main.js
@@ -1,18 +1,35 @@
 var typed = new Typed(".text", {
-
     strings: [
         "BIM Student",
         "Web Developer",
         "Frontend Developer",
         "Programmer"
     ],
-
     typeSpeed: 100,
-
     backSpeed: 70,
-
     backDelay: 1000,
-
     loop: true
-
 });
+
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
+window.onscroll = () => {
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+
+let darkModeIcon = document.querySelector('#darkMode-icon');
+
+darkModeIcon.onclick = () => {
+    darkModeIcon.classList.toggle('bx-sun');
+    document.body.classList.toggle('light-mode');
+};
